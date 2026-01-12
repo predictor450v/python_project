174703b044ee6d82df66948b87f5321942faa3d2
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-01-12 21:08:58 +0530

    finally it's working

--- 1_AI_powered_virtual_assistant/main.py
+++ 1_AI_powered_virtual_assistant/main.py
@@ -1,52 +1,54 @@
 import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3
-
-recognizer = sr.Recognizer()
-recognizer.energy_threshold = 3000
-recognizer.dynamic_energy_threshold = True
-
+import time
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)  
-
+recognizer = sr.Recognizer()
 
 def speak(text):
-    engine.say(text)   
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 170)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(text)
     engine.runAndWait()
+    engine.stop()
 
 
 if __name__ == "__main__":
     speak("Initializing virtual assistant jarvis")
+    time.sleep(1)
     
     
-    while True:
-        print("Say 'Jarvis' to activate...")
+while True:
+    print("Say 'Jarvis' to activate...")
+
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = recognizer.listen(source, timeout=2, phrase_time_limit=1)
         
-        try:
+        word = recognizer.recognize_google(audio).lower()
+        print(f"You said: {word}")
+        
+        if "jarvis" in word:
+            print("Wake word detected!")
+            time.sleep(0.5)  
+            speak("how can i help you")
+            time.sleep(0.5)  
+        
             with sr.Microphone() as source:
-                print("Listening...")
-                audio = recognizer.listen(source, timeout=2, phrase_time_limit=1)
+                print("jervis activeted...")
+                audio = recognizer.listen(source)
+                command = recognizer.recognize_google(audio).lower()
+                print(f"Command: {command}")
             
-            word = recognizer.recognize_google(audio).lower()
-            print(f"You said: {word}")
             
-            if "jarvis" in word:
-                print("Wake word detected!")
-                speak("how can i help you")
-           
-                with sr.Microphone() as source:
-                    print("jervis activeted...")
-                    audio = recognizer.listen(source)
-                    command = recognizer.recognize_google(audio).lower()
-                    print(f"Command: {command}")
-                
-                
-                # Process commands
-                
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print(f"Error: {e}")
-        except sr.WaitTimeoutError:
-            pass
+            # Process commands
+            
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print(f"Error: {e}")
+    except sr.WaitTimeoutError:
+        pass
