29a646c477f2b997acc9360284769131c89d29ee
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-01-12 19:33:44 +0530

    something happend

--- 1_AI_powered_virtual_assistant/main.py
+++ 1_AI_powered_virtual_assistant/main.py
@@ -1,7 +1,6 @@
 import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3
-import pocketsphinx
 
 recognizer =sr.Recognizer()
 engine = pyttsx3.init()
@@ -16,22 +15,29 @@ def speak(text):
 
 
 if __name__== "__main__":
-    speak("Initializing virtual assistant Sara....")
+    speak("Initializing virtual assistant jarvis....")
     # wake word sara
     
     while True:
         # obtain audio from the microphone
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source)
-
         # recognize speech using Google
+        r = sr.Recognizer()
+       
+        print("recogniging")
         try:
-            command =recognizer.recognize_google(audio)
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = recognizer.listen(source ,timeout =1 ,phrase_time_limit=1)
+            command =recognizer.recognize_google(audio).lower()
             print(command)
+            if "jarvis" in command:
+                speak("Yes, how can I help you?")
+
+
         except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
+            print("Could not understand audio")
         except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+            print(f"Error: {e}")
+        except sr.WaitTimeoutError:
+            print("Listening timed out")
 
